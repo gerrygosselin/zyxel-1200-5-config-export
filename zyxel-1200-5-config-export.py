738d470fdebed286ba3bb2494728deac95f41fef
Gerry Gosselin <ggosselin@gmail.com>
2022-06-25 21:13:57 -0400

    comments. renaming for clarification. additional error handling.

--- zyxel-1200-5-config-export.py
+++ zyxel-1200-5-config-export.py
@@ -1,9 +1,31 @@
 #!/usr/bin/env python3
 
+#
+# Config export script for zyxel 1200-5 switch.
+# Gerry Gosselin
+# Created 10/22/2021 - Initial commit.
+# Edited 6/25/2022 - Comments and some renaming for clarification.
+#
+# This script works on two types of files.
+# 1. A config backup file downloaded from the switch's web interface.
+# 2. A dump of the external flash ROM.
+#
+# This script extracts the admin password, IP address, subnet mask,
+# gateway, and hostname.
+#
+# A note about the admin password. It's encoded poorly. Multiple
+# characters encode to the same value. Therefore, when decodng
+# the password it may not look like what you expect but it works.
+#
+# Admin password encoding rules:
+# 1. case insensitive
+# 2. 5-9 and a-e encode to the sale value
+#
+
 import socket
 import sys
 
-cipher = [
+admin_pw_encoding = [
     '', '0', '1', '2', '3', '4', '5', '6',
     '7', '8', '9', 'f', 'g', 'h', 'i', 'j',
     'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
@@ -13,10 +35,14 @@ cipher = [
 if len(sys.argv) > 1:
     filename = sys.argv[1]
 else:
-    print("Provide a config backup or flash dump file as an argument.")
-    sys.exit()
+    sys.exit("Provide a config backup or flash dump file as an argument.")
 
 
+#
+# Decode admin password. Seek to the correct position.
+# Read 15 bytes. Decode characters and append to the
+# return value. 
+#
 def get_password(offset):
     config.seek(offset)
     pw_bytes = config.read(15)
@@ -24,23 +50,39 @@ def get_password(offset):
     password = ""
     for b in pw_bytes:
         if b==0: break
-        password += cipher[b]
+        password += admin_pw_encoding[b]
     return password
 
+
+#
+# Get IP Address (IP, netmask, or gateway).
+# Seek to the correct position. Read 4 bytes.
+# Convert ntoa and return.
+#
 def get_ipaddress(offset):
     config.seek(offset)
     return socket.inet_ntoa(config.read(4))
 
+
+#
+# Get hostname. Seek to the correct position.
+# Read 14 bytes. Decode to ascii and return.
+#
 def get_hostname(offset):
     config.seek(offset)
     return config.read(14).decode('ascii')
 
 
+#
+# Open config or flash file. Read the first 3 bytes.
+# If the bytes are 495449, it's a config backup.
+# If the bytes are 004002, it's a flash dump.
+# If the bytes are 123456, it's a firmware file (throw error).
+#
+# Seek to the correct position within the file
+# and start decoding the values.
+#
 with open(filename, "rb") as config:
-
-    # Determine if this is a config backup
-    # or a flash dump. Config backups start
-    # with the hex chars 49 54 49.
 
     start = config.read(3).hex()
 
@@ -58,5 +100,7 @@ with open(filename, "rb") as config:
         print("{0:13} {1}".format("Gateway:", get_ipaddress(2093069)))
         print("{0:13} {1}".format("Hostname:", get_hostname(2093079)))
 
+    elif start == "123456": # Firmware file
+        sys.exit("Firmware files don't have the current config. Choose a flash dump or config backup.")
     else:
-        print("Unknown binary file")
+        sys.exit("Unknown binary file")
